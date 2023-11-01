6033d5fd09417573960c72de06341aa498233701
Wallen Ribeiro <up202109260@g.uporto.pt>
2023-11-01 21:39:27 +0000

    connection closing

--- src/link_layer.c
+++ src/link_layer.c
@@ -240,31 +240,110 @@ int llopen(LinkLayer connectionParameters)
 ////////////////////////////////////////////////
 int llclose(int serialPortFd, int showStatistics)
 {
+
+    int STOP_M = FALSE;
+
+    enum State state;
+
+    state = START;
+
     if (serialPortFd < 0) {
         fprintf(stderr, "Serial port is not open\n");
         return -1;
     }
 
-    enum State state = START;
+    switch(linkerRole){
+        // Writer Role
+        case LlTx:
+        {
+            (void)signal(SIGALRM, alarmHandler);
+            while (numTries > 0 && STOP_M == FALSE) {
+                if (alarmEnabled == FALSE) {
+                    alarmHandler(timeout);
+                    alarmEnabled = TRUE;
+                }
 
+                unsigned char bytes[5] = {FLAG_BYTE, ADDR_SET, CTRL_DISC, BCC1(ADDR_SET, CTRL_DISC), FLAG_BYTE};
+                int x = write(serialPortFd, bytes, 5);
 
-    (void)signal(SIGALRM, alarmHandler);
+                sleep(1);
 
-    unsigned char bytes[5] = {FLAG_BYTE, ADDR_SET, CTRL_DISC, BCC1(ADDR_SET, CTRL_DISC), FLAG_BYTE};
-    int x = write(serialPortFd, bytes, 5);
-    if (x == -1) {
-        perror("Error writing to the serial port");
-        return -1;
-    }
-    int STOP_M = FALSE;
+                numTries = 3;
 
-    while (numTries > 0 && STOP_M == FALSE) {
-            if (alarmEnabled == TRUE)
-            {
-                alarm(timeout); 
-                alarmEnabled = FALSE;
+                if (x == -1) {
+                    perror("Error writing to the serial port");
+                    return -1;
+                }
+                while(alarmEnabled && !STOP_M){
+                    alarmEnabled = FALSE;
+                    int s = read(serialPortFd, &byte, 1);
+                    if (s){
+                        switch(state){
+                            case START:
+                                if (byte == FLAG_BYTE){
+                                    state = FLAG;                                }
+                                break;
+                            case FLAG:
+                                if (byte == ADDR_UA){
+                                    state = ADDR;
+                                }
+                                else if (byte != FLAG_BYTE){
+                                    state = START;
+                                }
+                                break;
+                            case ADDR:
+                                if (byte == CTRL_DISC){
+                                    state = CTRL;
+                                }
+                                else if (byte == FLAG_BYTE){
+                                    state = FLAG;
+                                }
+                                else{
+                                    state = START;
+                                }
+                                break;
+                            case CTRL:
+                                if (byte == BCC1(ADDR_UA, CTRL_DISC)){
+                                    state = BCC1;
+                                }
+                                else if (byte == FLAG_BYTE){
+                                    state = FLAG;
+                                }
+                                else{
+                                    state = START;
+                                }
+                                break;
+                            case BCC1:
+                                if (byte == FLAG_BYTE){
+                                    STOP_M = TRUE;
+                                    alarmEnabled = FALSE;
+                                    break;
+                                }
+                                else{
+                                    state = START;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                numTries--;
             }
-            while(!STOP_M && !alarmEnabled){
+            if (!numTries) printf("No more tries\n");
+            if (STOP_M != TRUE) return -1;
+
+            int send = sendControlPacket(serialPortFd, CTRL_UA);
+            if (send == -1){
+                printf("Error sending UA packet\n");
+                return -1;
+            }
+            break;
+        }
+        // Reader Role
+        case LlRx:
+        {
+            while(!STOP_M){
                 int s = read(serialPortFd, &byte, 1);
                 if (s){
                     switch(state){
@@ -274,7 +353,7 @@ int llclose(int serialPortFd, int showStatistics)
                             }
                             break;
                         case FLAG:
-                            if (byte == ADDR_UA){
+                            if (byte == ADDR_SET){
                                 state = ADDR;
                             }
                             else if (byte != FLAG_BYTE){
@@ -293,7 +372,7 @@ int llclose(int serialPortFd, int showStatistics)
                             }
                             break;
                         case CTRL:
-                            if (byte == BCC1(ADDR_UA, CTRL_DISC)){
+                            if (byte == BCC1(ADDR_SET, CTRL_DISC)){
                                 state = BCC1;
                             }
                             else if (byte == FLAG_BYTE){
@@ -311,30 +390,21 @@ int llclose(int serialPortFd, int showStatistics)
                                 state = START;
                             }
                             break;
-                        default:
-                            break;
+                            default:
+                                break;
                         }
                     }
                 }
-                numTries--;
-            }
-    if (STOP_M != TRUE) return -1;
-    unsigned char bytes_ua[5] = {FLAG_BYTE, ADDR_UA, CTRL_UA, BCC1(ADDR_UA, CTRL_UA), FLAG_BYTE};
-    write(serialPortFd, bytes_ua, 5);
-    
-    if (close(serialPortFd) < 0) {
-        perror("Error closing serial port");
-        return -1;
+
+                int send = sendControlPacket(serialPortFd, CTRL_DISC);
+                if (send == -1){
+                    printf("Error sending UA packet\n");
+                    return -1;
+                }
+        }
     }
 
-    if (showStatistics){
-        printf("Statistics:\n"
-               "  - Sent %d frames\n"
-               "  - Received %d frames\n"
-               "  - Rejected %d frames%d\n"
-               "  - Timeouts: %d\n",
-               0, 0, 0, 0);
-    }
 
+    printf("Connection TERMINATED\n");
     return 0;
 }
